cb18bda60674973f41e61df7b7d7c64c817fca07
deptno <deptno@gmail.com>
2023-09-18 00:18:06 +0900

    update yamlls + yaml-companion config

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -1,3 +1,4 @@
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 local configs = require("plugins.configs.lspconfig")
 local on_attach = configs.on_attach
 local capabilities = configs.capabilities
@@ -15,5 +16,46 @@ for _, lsp in ipairs(servers) do
   }
 end
 
-local cfg = require("yaml-companion").setup()
+local cfg = require("yaml-companion").setup({
+  -- Built in file matchers
+  builtin_matchers = {
+    -- Detects Kubernetes files based on content
+    kubernetes = { enabled = true },
+    cloud_init = { enabled = true }
+  },
+
+  -- Additional schemas available in Telescope picker
+  schemas = {
+    {
+      name = "Kubernetes 1.26,0",
+      uri = "https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.26.0-standalone-strict/all.json",
+    },
+    {
+      name = "Azure pipeline",
+      uri = "https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/master/service-schema.json"
+    }
+  },
+
+  -- Pass any additional options that will be merged in the final LSP config
+  lspconfig = {
+    flags = {
+      debounce_text_changes = 150,
+    },
+    settings = {
+      redhat = { telemetry = { enabled = false } },
+      yaml = {
+        validate = true,
+        format = { enable = true },
+        hover = true,
+        schemaStore = {
+          enable = true,
+          url = "https://www.schemastore.org/api/json/catalog.json",
+        },
+        schemaDownload = { enable = true },
+        schemas = {},
+        trace = { server = "debug" },
+      },
+    },
+  },
+})
 lspconfig.yamlls.setup(cfg)
