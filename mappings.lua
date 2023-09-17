d175a536c2353b368e1b4a23545293c6541d4299
deptno <deptno@gmail.com>
2023-09-17 23:07:07 +0900

    add gitsigns key mappings

--- mappings.lua
+++ mappings.lua
@@ -6,6 +6,10 @@ M.disabled = {
     -- nvimtree
     ["<C-n>"] = "",
     ["<leader>e"] = "",
+    ["<leader>rh"] = "",
+    ["<leader>ph"] = "",
+    ["<leader>gb"] = "",
+    ["<leader>td"] = "",
   }
 }
 
@@ -14,6 +18,62 @@ M.nvimtree = {
   n = {
      ["sf"] = { "<cmd> NvimTreeToggle <CR>", "Toggle nvimtree" },
   },
+}
+M.gitsigns = {
+  n = {
+    ["]c"] = {
+      function()
+        if vim.wo.diff then
+          return "]c"
+        end
+        vim.schedule(function()
+        local gs = require("gitsigns")
+        gs.next_hunk()
+        end)
+        return "<Ignore>"
+      end,
+      "Jump to next hunk",
+      opts = { expr = true },
+    },
+    ["[c"] = {
+      function()
+        if vim.wo.diff then
+          return "[c"
+        end
+        vim.schedule(function()
+          local gs = require("gitsigns")
+          gs.prev_hunk()
+        end)
+        return "<Ignore>"
+      end,
+      "Jump to prev hunk",
+      opts = { expr = true },
+    },
+    [",r"] = {
+      function()
+        require("gitsigns").reset_hunk()
+      end,
+      "Reset hunk",
+    },
+    [",."] = {
+      function()
+        require("gitsigns").preview_hunk()
+      end,
+      "Preview hunk",
+    },
+    [",,"] = {
+      function()
+        package.loaded.gitsigns.blame_line()
+      end,
+      "Blame line",
+    },
+    [",t"] = {
+      function()
+        require("gitsigns").toggle_deleted()
+      end,
+      "Toggle deleted",
+    },
+  }
 }
 
 return M
